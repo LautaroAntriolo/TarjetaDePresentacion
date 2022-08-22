4894a9602d6012dfa1d153caf9e37a0311896a64
LautaroAntriolo <lautaroantriololuis@gmail.com>
2022-08-22 07:07:14 -0300

    primera parte

---
+++ js/move.js
@@ -0,0 +1,19 @@
+// let imagen = document.querySelector(".main__img")
+// let titulo = document.querySelector(".main__title")
+// let parrafo = document.querySelector(".main__p")
+// let boton = document.querySelector(".mascota")
+
+// boton.addEventListener('mouseover', () => {
+//     titulo.textContent = "Dina Antriolo"
+//     parrafo.textContent = 'Tengo 4 años, soy impulsiva y duermo adentro con ropita nueva. Extraño mucho a mi familia que no vive en casa. Me gusta ensuciar el patio y ya no rompo las plantas'
+//     imagen.src = "./img/DinaPerfil.png"
+
+// });
+
+// boton.addEventListener('mouseout', () => {
+//     titulo.textContent = "Lautaro Antriolo"
+//     parrafo.textContent = 'Tengo 25 años, soy estudiante de Biología y un apasionado por la programación. Me interesa Python, React, Node.js y Docker. Mi próxima área de estudio será Google Cloud o AWS.'
+//     imagen.src = "./img/LautaroAntrioloPerfil.png"
+//     imagen.className += "animated infinite pulse delay-2s";
+// });
+
